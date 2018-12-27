5626145d7ccc5533ae437941552b310d40413d21
Bogdan Lomako <lomakojob@gmail.com>
2018-12-27 19:16:40 +0200

    Added link to about on home page

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import path
+from django.urls import path
 from rango import views
 
 urlpatterns = [
-    path(r'^$', views.index, name = 'index'),
+    path('', views.index, name = 'index'),
+    path('about/', views.about, name ='about'),
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Simple Output")
+    path = request.path
+    return HttpResponse('<a href="/rango/about/">About</a>')
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
+
