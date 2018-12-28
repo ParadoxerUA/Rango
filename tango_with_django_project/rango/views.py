dba2d1659b9da47207d2e32a895d6afccd620e2a
Bogdan Lomako <lomakojob@gmail.com>
2018-12-28 15:35:43 +0200

    changed index view by using template, added TAMPLETES_DIR to settings.py

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    path = request.path
-    return HttpResponse('<p>you are on the home page</p>'
-                        '<a href="/rango/about/">About</a>')
+    context_dict = {'boldmessage': "My Tamplate, embedding python vars \
+		            in html is easy "}
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return HttpResponse('<p>about rango...</p>'
