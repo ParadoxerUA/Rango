901221633adefc36b516a3bb0e58888c6f57a7a1
Bogdan Lomako <lomakojob@gmail.com>
2019-01-02 23:36:46 +0200

    Implemented 'Add page' form

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -21,3 +21,12 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
+
+    # def clean(self):
+    #     cleaned_data = self.cleaned_data
+    #     url = cleaned_data.get('url')
+    #     if url and not url.startswith('http://'):
+    #         url = 'http://' + url
+    #         cleaned_data['url'] = url
+    #
+    #         return cleaned_data
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('about/', views.about, name ='about'),
     path('add_category/', views.add_category, name='add_category'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('category/<slug:category_name_slug>', views.show_category, name='show_category')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
