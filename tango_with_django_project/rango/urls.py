d52afaa649bbfa2fd3b2d8d50b8af1fb36727155
Bogdan Lomako <lomakojob@gmail.com>
2019-01-21 22:12:37 +0200

    added rango url namespacing

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,6 +3,7 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'rango'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('about/', views.about, name ='about'),
